89da9550a706d92cefb7e2386bbac94e815e352f
Professional <Professional@USER-PC>
2022-01-11 01:07:07 +0300

    Добавьте файлы проекта.

---
+++ SpeechToText/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SpeechToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(253, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите аудиофайл";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(31, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выбрать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IBM Watson";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Pink;
+            this.listBox1.Location = new System.Drawing.Point(231, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(769, 121);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(414, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(574, 106);
+            this.textBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Pink;
+            this.listBox2.Location = new System.Drawing.Point(231, 167);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(769, 121);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(253, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 42);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Преобразовать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = " Google Cloud Platform";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(414, 173);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(574, 106);
+            this.textBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.Pink;
+            this.listBox3.Location = new System.Drawing.Point(231, 322);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(769, 121);
+            this.listBox3.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Location = new System.Drawing.Point(253, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 42);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Преобразовать";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Microsoft Azure";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(414, 325);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(574, 106);
+            this.textBox3.TabIndex = 14;
+            // 
+            // listBox4
+            // 
+            this.listBox4.BackColor = System.Drawing.Color.Pink;
+            this.listBox4.Location = new System.Drawing.Point(231, 491);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(769, 121);
+            this.listBox4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(287, 531);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Speechtext.ai";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Location = new System.Drawing.Point(253, 560);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 42);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Преобразовать";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(414, 496);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(574, 106);
+            this.textBox4.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1033, 706);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ SpeechToText/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.SpeechToText.v1;
+using Newtonsoft.Json.Linq;
+using Google.Cloud.Speech.V1;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using NAudio.Wave;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SpeechToText
+{
+    public partial class Form1 : Form
+    {
+        string filepath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Файлы mp3|*.mp3";
+            if (OPF.ShowDialog() == DialogResult.OK) {filepath = OPF.FileName;}
+        }
+
+        //IBM Watson
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            string key = "weUJxiXv-R9A96AuovjTWhqKuRl25jpIb-SV4ebvh24t";
+            string url = "https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/d06d44af-8045-4752-852f-832c674d4de9";
+
+            IamAuthenticator authenticator = new IamAuthenticator(apikey: key);
+
+            SpeechToTextService speechToText = new SpeechToTextService(authenticator);
+            speechToText.SetServiceUrl(url);
+
+            var result = speechToText.Recognize(
+                audio: new MemoryStream(File.ReadAllBytes(filepath)),
+                contentType: "audio/mp3",
+                endOfPhraseSilenceTime: 10
+                );
+            JObject json = JObject.Parse(result.Response);
+            textBox1.AppendText(json["results"][0]["alternatives"][0]["transcript"].ToString());
+        }
+
+        //Google Cloud Platform
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            var speech = new SpeechClientBuilder
+            {
+                CredentialsPath = "client_secret2.json"
+            }.Build();
+
+            var response = speech.Recognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.EncodingUnspecified,
+                SampleRateHertz = 48000,
+                LanguageCode = "en",
+            }, RecognitionAudio.FromFile(filepath));
+
+            textBox2.Text = "";
+
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    textBox2.Text = textBox2.Text + " " + alternative.Transcript;
+                }
+            }
+        }
+
+        //Microsoft Azure
+        string result123;
+        public async Task ContinuousRecognitionWithFileAsync()
+        {
+            var config = SpeechConfig.FromSubscription("289c88a602f64297a01812078354facf", "eastus");
+            var stopRecognition = new TaskCompletionSource<int>();
+            //MP3 to WAV
+            string tempFileName = Path.GetTempFileName();
+            Mp3FileReader mp3FileReader = new Mp3FileReader(filepath);
+            WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3FileReader);
+            WaveFileWriter.CreateWaveFile(tempFileName, pcm);
+
+            using (var audioInput = AudioConfig.FromWavFileInput(tempFileName))
+            {
+                using (var recognizer = new SpeechRecognizer(config, audioInput))
+                {
+                    // конечный результат
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            result123 += e.Result.Text;
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            result123 = "Speech could not be recognized.";
+                        }
+                    };
+
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                    await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        async private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            result123 = "";
+            await ContinuousRecognitionWithFileAsync();
+            textBox3.Text = textBox3.Text + " " + result123;
+        }
+
+        //SpeechText.AI
+        string resp;
+        public async Task SpeechApi()
+        {
+            string url = "https://api.speechtext.ai/recognize?key=ec3275eab16f41c8aa6b5e4b238ef64e&language=en-US&punctuation=true&format=mp3";
+
+            HttpClient client = new HttpClient();
+
+            var content = new MultipartFormDataContent();
+            var audioContent = new ByteArrayContent(System.IO.File.ReadAllBytes(filepath));
+            audioContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/3gpp");
+            content.Add(audioContent, "audio", "testaudio.3gpp");
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
+
+            var httpResponseMessage = await client.PostAsync(url, content);
+
+            var response = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            JObject json = JObject.Parse(response);
+            var task = json["id"].ToString();
+
+            string newurl = "https://api.speechtext.ai/results?key=ec3275eab16f41c8aa6b5e4b238ef64e" + "&task=" + task + "&summary=true&summary_size=15&highlights=true&max_keywords=15";
+            while (true)
+            {
+                var httpResponseMessage2 = await client.GetAsync(newurl);
+                var response2 = await httpResponseMessage2.Content.ReadAsStringAsync();
+                JObject json2 = JObject.Parse(response2);
+
+                if (json2["status"].ToString() == "failed")
+                {
+                    resp = "Failed to transcribe!";
+                    break;
+                }
+                if (json2["status"].ToString() == "finished")
+                {
+                    resp = json2["results"]["transcript"].ToString().Replace("<kw>", "").Replace("</kw>", "");
+                    break;
+                }
+                // sleep for 15 seconds if the task has the status - 'processing'
+                System.Threading.Thread.Sleep(15);
+            }
+
+        }
+        async private void button5_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            await SpeechApi();
+            textBox4.Text = textBox4.Text + " " + resp;
+        }
+    }
+}
+
+    
+    
+
